d187af63d3b64345fdf02930f70b1066047ab8b0
10123815 <ysdysdysdysd123@163.com>
2016-03-28 13:47:00 +0800

    cpp concurrency study

---
+++ test.cc
@@ -0,0 +1,21 @@
+#include <thread>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	auto func_obj = [](int num, int& res)
+	{
+		res += num;
+		std::cout<<num<<"  "<<res<<std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	};
+
+	int res = 0;
+	int cmd = 0;
+	while (std::cin>>cmd)
+	{
+		std::thread t(func_obj, cmd, std::ref(res));
+		t.detach();
+	}
+
+}
