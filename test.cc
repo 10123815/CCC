62c29d1001f81cb606463f39f134c8a741177c40
10123815 <ysdysdysdysd123@163.com>
2016-04-01 20:54:44 +0800

    unique_lock

--- test.cc
+++ test.cc
@@ -1,22 +1,59 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 #include <mutex>
+
+std::mutex mtx;
+
+std::string data = "111111111111111";
+
+class Resource
+{
+public:
+	void Use (int user_id) 
+	{
+		std::unique_lock<std::mutex> lck(mtx_);
+		DoUse(user_id);
+	}
+protected:
+	virtual void DoUse(int user_id) = 0;
+private:
+	std::mutex mtx_;
+};
+
+class StringResource : public Resource
+{
+public:
+	StringResource (const std::string& str) : str_(str) {}
+protected:
+	void DoUse(int user_id) override
+	{
+		for (auto& ch : str_)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			std::cout<<"    "<<user_id<<" use "<<ch<<std::endl;
+		}
+	}
+private:
+	std::string str_;
+};
+
+void UseResource(Resource* rsrc, int id)
+{
+	rsrc->Use(id);
+}
 
 int main(int argc, char const *argv[])
 {
-	auto func_obj = [](int num, int& res)
-	{
-		res += num;
-		std::cout<<num<<"  "<<res<<std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	};
-
-	int res = 0;
-	int cmd = 0;
+	
+	Resource* rsrc = new StringResource(data);
+	int cmd;
 	while (std::cin>>cmd)
 	{
-		std::thread t(func_obj, cmd, std::ref(res));
-		t.detach();
+		std::thread thr1(UseResource, rsrc, std::ref(cmd));
+		thr1.detach();
 	}
 
 }
