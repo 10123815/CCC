fd99bf5349233a29d11b2b8193a32407199c16f6
10123815 <ysdysdysdysd123@163.com>
2016-04-01 21:48:53 +0800

    unique_lock

--- test.cc
+++ test.cc
@@ -1,44 +1,8 @@
-#include <thread>
-#include <iostream>
-#include <vector>
 #include <string>
-#include <fstream>
-#include <mutex>
 
-std::mutex mtx;
+#include "multi_thread_resource.h"
 
 std::string data = "111111111111111";
-
-class Resource
-{
-public:
-	void Use (int user_id) 
-	{
-		std::unique_lock<std::mutex> lck(mtx_);
-		DoUse(user_id);
-	}
-protected:
-	virtual void DoUse(int user_id) = 0;
-private:
-	std::mutex mtx_;
-};
-
-class StringResource : public Resource
-{
-public:
-	StringResource (const std::string& str) : str_(str) {}
-protected:
-	void DoUse(int user_id) override
-	{
-		for (auto& ch : str_)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			std::cout<<"    "<<user_id<<" use "<<ch<<std::endl;
-		}
-	}
-private:
-	std::string str_;
-};
 
 void UseResource(Resource* rsrc, int id)
 {
@@ -48,12 +12,21 @@ void UseResource(Resource* rsrc, int id)
 int main(int argc, char const *argv[])
 {
 	
-	Resource* rsrc = new StringResource(data);
+	Resource* file_rsrc = new FileResource("a");
+	Resource* str_rsrc = new StringResource(data);
 	int cmd;
 	while (std::cin>>cmd)
 	{
-		std::thread thr1(UseResource, rsrc, std::ref(cmd));
-		thr1.detach();
+		if (cmd % 2 == 0)
+		{
+			std::thread thr(UseResource, file_rsrc, std::ref(cmd));
+			thr.detach();
+		}
+		else
+		{
+			std::thread thr(UseResource, str_rsrc, std::ref(cmd));
+			thr.detach();
+		}
 	}
 
 }
